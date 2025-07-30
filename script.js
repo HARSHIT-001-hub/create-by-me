1932298cd90284a5b34d6e11703b65751195fb41
Harshit Tripathi <htripathi9324@gmail.com>
2025-07-30 20:55:49 +0530

    new js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const mydetail = {
+    name: "Harshit", //string
+    age: 20, //number
+    city: "Mumbai", //string
+    isStudent: true, //boolean
+    hobbies: ["coding","gaming"] // Array of hobbies
+};
+
+console.log(mydetail);

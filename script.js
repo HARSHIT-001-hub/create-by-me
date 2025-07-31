2a8e29bfca495d48f050c916a988c6ef66138296
Harshit Tripathi <htripathi9324@gmail.com>
2025-07-31 20:05:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,20 +24,32 @@ console.log(mydetail);
 // console.log(a /= 2) // a = a / 2
 
 //Conditional operator
-//if statement
-let age = 20
-if (age > 18){
-    console.log("Allow to Vote");
-}else{
-    console.log("Not Allow to Vote");
+// //if statement
+// let age = 20
+// if (age > 18){
+//     console.log("Allow to Vote");
+// }else{
+//     console.log("Not Allow to Vote");
+// }
+// let marks = 75;
+// if (marks >= 90) {
+//     console.log("Grade A");
+// }else if (marks >= 75) {
+//     console.log("Grade B");
+// }else if (marks >= 50) {
+//     console.log("Grade C");
+// }else {
+//     console.log("Grade D");
+// }
+
+// age = 19 ? "adult": "minor";
+// console.log(age);
+
+prompt("Enter your age: ");
+let userAge = parseInt(prompt("Enter your age: "));
+if (userAge >= 18) {
+    console.log("You are eligible to vote.");
 }
-let marks = 75;
-if (marks >= 90) {
-    console.log("Grade A");
-}else if (marks >= 75) {
-    console.log("Grade B");
-}else if (marks >= 50) {
-    console.log("Grade C");
-}else {
-    console.log("Grade D");
+else {
+    console.log("You are not eligible to vote.");
 }

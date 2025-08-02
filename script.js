604eb9c31bf15cae5027bbb5625abc1da66abbd4
Harshit Tripathi <htripathi9324@gmail.com>
2025-08-02 19:40:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -97,13 +97,13 @@ console.log(mydetail);
 
 // New Task
 
-function countvowel(str){
-    let count = 0;
-    for (const char of str){
-        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u");
-    {
-        count++;
-    }
-    return count;
-};
-}
+// function countvowel(str){
+//     let count = 0;
+//     for (const char of str){
+//         if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+//         count++;
+//     }
+//     };
+//     return count;
+// }
+
